9ff83004901fc83189bf9eda11578b6f29bd78f5
Gerhardt Datsomor <datsomorg@gmail.com>
2026-01-23 13:28:09 +0000

    fix cors issues

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*","https://nouritrack-backend-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
